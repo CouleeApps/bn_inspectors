379410000f81fa345911ff9005d212a426743d65
Glenn Smith <glenn@vector35.com>
2024-04-10 17:13:21 -0400

    And debug printing

--- __init__.py
+++ __init__.py
@@ -8,7 +8,9 @@ if __name__ != '__main__':
         try:
             if (file.endswith('.py') and file != '__init__.py'):
                 importlib.import_module(f'{__name__}.{file[:-3]}')
+                print(f"Import submodule {__name__}.{file[:-3]}")
             elif os.path.isdir(full) and os.path.exists(f"{full}/__init__.py"):
                 importlib.import_module(f'{__name__}.{file}')
+                print(f"Import submodule {__name__}.{file}")
         except:
             traceback.print_exc()
