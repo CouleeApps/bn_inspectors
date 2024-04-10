84b74937bf51abda492b2007280d48e53430034c
Glenn Smith <glenn@vector35.com>
2024-04-10 17:12:05 -0400

    Base script error handling

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 import importlib
 import os
+import traceback
 
 if __name__ != '__main__':
     for file in os.listdir(os.path.dirname(__file__)):
         full = os.path.join(os.path.dirname(__file__), file)
-        if (file.endswith('.py') and file != '__init__.py'):
-            importlib.import_module(f'{__name__}.{file[:-3]}')
-        elif os.path.isdir(full) and os.path.exists(f"{full}/__init__.py"):
-            importlib.import_module(f'{__name__}.{file}')
+        try:
+            if (file.endswith('.py') and file != '__init__.py'):
+                importlib.import_module(f'{__name__}.{file[:-3]}')
+            elif os.path.isdir(full) and os.path.exists(f"{full}/__init__.py"):
+                importlib.import_module(f'{__name__}.{file}')
+        except:
+            traceback.print_exc()
