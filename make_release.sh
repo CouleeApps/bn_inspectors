5952677521798253239917944497041398539181
Glenn Smith <glenn@vector35.com>
2025-01-02 17:37:00 -0500

    Fancier bash

--- make_release.sh
+++ make_release.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
 set -e
+#set -x
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <release name>"
+  echo "Flatten submodules in current tree, commit results, and then"
+  echo "create and push a tag with the given name (and description)"
   exit 1
 fi
 
@@ -12,6 +15,7 @@ RELEASE_NAME="$(echo "$1" | sed "s/^v//")"
 BASE_COMMIT=$(git rev-parse HEAD)
 # Thanks, xonsh
 PREVIOUS_BRANCH=$(git symbolic-ref --short HEAD)
+RELEASE_BRANCH_NAME="_release"
 
 # Thanks, xonsh
 if [ "X$(git status --porcelain --untracked-files=no --ignore-submodules=untracked)" != "X" ]; then
@@ -20,13 +24,14 @@ if [ "X$(git status --porcelain --untracked-files=no --ignore-submodules=untrack
 fi
 
 # Switch to release branch and update it to our base commit
-git switch -qC release $BASE_COMMIT
+git switch -qC $RELEASE_BRANCH_NAME $BASE_COMMIT
 
 # De-submodule all the submodules
 SUBMODULES=$(git ls-tree -r HEAD "--format=%(path) %(objecttype)" | grep commit | cut -f1 "-d ")
 
 recover() {
   # Put the .git file back so git knows where they go
+  # Iterate the list https://superuser.com/a/284226
   while IFS= read -r SUBMODULE || [[ -n "$SUBMODULE" ]]; do
     # todo: recurse submodules?
     # Gross https://stackoverflow.com/a/7305217
@@ -35,26 +40,28 @@ recover() {
   done < <(printf '%s' "$SUBMODULES")
 
   # And now we can switch branches apparently totally fine
-  git switch "$PREVIOUS_BRANCH"
+  git switch -q "$PREVIOUS_BRANCH"
 
   # And update them just in case
+  # https://superuser.com/a/284226
   while IFS= read -r SUBMODULE || [[ -n "$SUBMODULE" ]]; do
     # todo: recurse submodules?
     git submodule update --init --recursive -- "$SUBMODULE"
   done < <(printf '%s' "$SUBMODULES")
+
+  git branch -qD "$RELEASE_BRANCH_NAME"
 }
 
-# Iterate the list https://superuser.com/a/284226
+# https://superuser.com/a/284226
 while IFS= read -r SUBMODULE || [[ -n "$SUBMODULE" ]]; do
   # todo: recurse submodules?
-  git rm --cached "$SUBMODULE"
+  git rm -q --cached "$SUBMODULE"
   rm "$SUBMODULE/.git"
   git add "$SUBMODULE"
 done < <(printf '%s' "$SUBMODULES")
 
-git commit -m "Flattened submodules for release v${RELEASE_NAME}" || (recover && exit 1)
+git commit -qm "Flattened submodules for release v${RELEASE_NAME}" || (recover && exit 1)
 git tag "$RELEASE_NAME" -s || (recover && exit 1)
-git push -f origin release || (recover && exit 1)
 git push origin "refs/tags/${RELEASE_NAME}" || (recover && exit 1)
 # todo: make github release?
 
